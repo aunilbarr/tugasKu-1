b3559510e32a061ec8b1e7369861cdc72edb9806
aunilbarr <61440644+aunilbarr@users.noreply.github.com>
2021-01-07 16:06:02 +0700

    fixed the migrations
    string deadline date & time

--- database/migrations/2021_01_06_160448_create_tugas_table.php
+++ database/migrations/2021_01_06_160448_create_tugas_table.php
@@ -15,11 +15,11 @@ class CreateTugasTable extends Migration
     {
         Schema::create('tugas', function (Blueprint $table) {
             $table->id();
-            $table->integer('account_id')->index();
+            $table->integer('account_id');
             $table->string('subject', 50)->unique();
             $table->string('abbrev', 150)->unique();
-            $table->timestamps('deadline_date');
-            $table->timestamps('deadline_time');
+            $table->string('deadline_date');
+            $table->string('deadline_time');
             $table->string('group', 50)->nullable();
             $table->string('file_attachments', 100)->nullable();
             $table->string('details', 150)->nullable();
